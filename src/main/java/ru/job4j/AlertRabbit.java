a1fecb9001ad55ffb0ad8e2741bcaf156381c007
IvanPavlovets <ivan150287@gmail.com>
2021-05-07 09:39:37 +0700

    1. Quartz [#175122]

---
+++ src/main/java/ru/job4j/AlertRabbit.java
@@ -0,0 +1,46 @@
+package ru.job4j;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import static org.quartz.JobBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+
+/**
+ * 1) Конфигурирование и начало работы происходит с создания экземпляра класс планировщика - Scheduler,
+ * управляющего всеми работами. В объект Scheduler мы будем добавлять задачи, которые хотим выполнять периодически.
+ * 2) Создание задачи происходит в JobDetail.
+ * 3) Создание расписания с указанием переодичности запуска происходит в SimpleScheduleBuilder.
+ * 4) Задача выполняется через триггер. Тригер определяет когда запускаеться конкретное задание.
+ * 5) После создания задачи и триггера, эти компоненты загружаються в планировщик.
+ */
+public class AlertRabbit {
+    public static void main(String[] args) {
+        ConfigValues config = new ConfigValues("rabbit.properties");
+        int interval = Integer.parseInt(config.get("rabbit.interval"));
+        Scheduler scheduler = null;
+        try {
+            scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = newJob(Rabbit.class).build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(interval).repeatForever();
+            Trigger trigger = newTrigger().startNow().withSchedule(times).build();
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Внутриний класс задачи, реализующий интерфейс Job.
+     */
+    public static class Rabbit implements Job {
+        @Override
+        public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+            System.out.println("Rabbit runs here ...");
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/ConfigValues.java
@@ -0,0 +1,43 @@
+package ru.job4j;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Класс подключения к настройкам файла .properties.
+ */
+
+public class ConfigValues {
+    private final Properties prop = new Properties();
+
+    private final String fileName;
+
+    public ConfigValues(String fileName) {
+        this.fileName = fileName;
+        this.loadPropValues();
+    }
+
+    /**
+     * Метод считывает пары строк (key/value) из файла .properties в свою внутрюнию карту.
+     */
+    public void loadPropValues() {
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(fileName)) {
+            prop.load(inputStream);
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    /**
+     * Метод достает искомый парметр из внутреней карты, по ключу.
+     *
+     * @param key
+     * @return
+     */
+    public String get(String key) {
+        return this.prop.getProperty(key);
+    }
+}
+
+
+
